fe7385c1981d1b1be63806eaa8ffd2266c3e5f9d
Alexandros Schillings <aschillings@gmail.com>
2019-12-23 12:17:03 +0000

    Fix crash if bluetoothclass is unavailable

--- app/src/main/java/uk/co/alt236/bluetoothconnectionlog/receiver/DataMapper.kt
+++ app/src/main/java/uk/co/alt236/bluetoothconnectionlog/receiver/DataMapper.kt
@@ -14,7 +14,7 @@ class DataMapper {
         val btDevice = BtDevice(
             name = device.name ?: "",
             macAddress = device.address,
-            bluetoothClass = BluetoothClass(device.bluetoothClass),
+            bluetoothClass = getBluetoothClass(device),
             type = DeviceType.fromAndroidType(device.type)
         )
 
@@ -30,15 +30,19 @@ class DataMapper {
         logEntry: LogEntry,
         androidLocation: android.location.Location
     ): LogEntry {
-        val location = Location(
-            latitude = androidLocation.latitude,
-            longitude = androidLocation.longitude,
-            altitude = androidLocation.altitude,
+        val location = if (androidLocation.latitude == 0.0 && androidLocation.longitude == 0.0) {
+            Location.INVALID
+        } else {
+            Location(
+                latitude = androidLocation.latitude,
+                longitude = androidLocation.longitude,
+                altitude = androidLocation.altitude,
 
-            timestamp = androidLocation.time,
-            horizontalAccuracy = androidLocation.accuracy,
-            verticalAccuracy = getVerticalAccuracy(androidLocation)
-        )
+                timestamp = androidLocation.time,
+                horizontalAccuracy = androidLocation.accuracy,
+                verticalAccuracy = getVerticalAccuracy(androidLocation)
+            )
+        }
 
         return LogEntry(
             timestamp = logEntry.timestamp,
@@ -48,11 +52,26 @@ class DataMapper {
         )
     }
 
+    private fun getBluetoothClass(device: BluetoothDevice): BluetoothClass {
+        return if (device.bluetoothClass == null) {
+            INVALID_BLUETOOTH_CLASS
+        } else {
+            BluetoothClass(device.bluetoothClass)
+        }
+    }
+
     private fun getVerticalAccuracy(androidLocation: android.location.Location): Float {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             androidLocation.verticalAccuracyMeters
         } else {
             0.0f
         }
+    }
+
+    private companion object {
+        val INVALID_BLUETOOTH_CLASS = BluetoothClass(
+            deviceClass = android.bluetooth.BluetoothClass.Device.Major.UNCATEGORIZED,
+            majorDeviceClass = android.bluetooth.BluetoothClass.Device.Major.UNCATEGORIZED
+        )
     }
 }
